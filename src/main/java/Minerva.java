43c24f64abc7b710bdef03c211ee07d1eeab53de
Wei Xian <howeixian2688@gmail.com>
2026-08-19 05:03:01 +0800

    Completed level-0

---
+++ src/main/java/Minerva.java
@@ -0,0 +1,16 @@
+public class Minerva {
+    public static void main(String[] args) {
+        String banner = "$$\\      $$\\       $$\\                                                                         $$$$$$\\  \n"
+                + "$$$\\    $$$ |      \\__|                                                                       $$  __$$\\ \n"
+                + "$$$$\\  $$$$ |      $$\\       $$$$$$$\\         $$$$$$\\         $$$$$$\\        $$\\    $$\\       $$ /  $$ |\n"
+                + "$$\\$$\\$$ $$ |      $$ |      $$  __$$\\       $$  __$$\\       $$  __$$\\       \\$$\\  $$  |      $$$$$$$$ |\n"
+                + "$$ \\$$$  $$ |      $$ |      $$ |  $$ |      $$$$$$$$ |      $$ |  \\__|       \\$$\\$$  /       $$  __$$ |\n"
+                + "$$ |\\$  /$$ |      $$ |      $$ |  $$ |      $$   ____|      $$ |              \\$$$  /        $$ |  $$ |\n"
+                + "$$ | \\_/ $$ |      $$ |      $$ |  $$ |      \\$$$$$$$\\       $$ |               \\$  /         $$ |  $$ |\n"
+                + "\\__|     \\__|      \\__|      \\__|  \\__|       \\_______|      \\__|                \\_/          \\__|  \\__|\n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Minerva! Ask me anything.");
+        System.out.println("What can I do for you today :)?");
+        System.out.println("Bye. And I hope to see you again soon!");
+    }
+}
