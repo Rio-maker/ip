f29a5a384a83eedc08d729ee8d60a6ebbf670c0c
Wei Xian <howeixian2688@gmail.com>
2026-08-19 14:48:12 +0800

    Complete level-2 Add, List

--- src/main/java/Minerva.java
+++ src/main/java/Minerva.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Minerva {
+    private static int counter = 0;
+    public static String[] taskList = new String[100];
     public static void showFarewell() {
         System.out.println("Bye. And I hope to see you again soon!");
     }
     public static void showGreeting() {
         System.out.println("Hello! I'm Minerva! Ask me anything.");
+    }
+    public static void showTasklist() {
+        for (int i = 0; i < counter; i++) {
+            System.out.println((i + 1) + ". " + taskList[i]);
+        }
     }
     public static void taskLoop(Scanner sc) {
         while(true) {
@@ -13,10 +21,15 @@ public class Minerva {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                showTasklist();
+            } else {
+                taskList[counter] = input;
+                counter ++;
+                System.out.println("added: " + input);
             }
-            System.out.println("Roger, I will " + input);
+            }
         }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String banner = "$$\\      $$\\       $$\\                                                                         $$$$$$\\  \n"
